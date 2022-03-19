92e103fea21f7e6c11b48c8c8131db0ca6f02908
Luke Lynch <Tech.Luk3@gmail.com>
2022-03-20 01:19:42 +1100

    Changes: * Added Insomnia collection * Added new endpoints * renamed hook link * added dependency * added typeacquisitions

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,29 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+import fs from 'fs';
+
+const publicKey = fs.readFileSync('secrets/jwtRS256.key.pub').toString();
+
 
 const app = express();
+app.use(morgan('combine'));
+app.use(bodyParser.json());
+app.use(cors())
 
+app.get('/health', (req, res) => {
+    res.status = 200;
+    res.send({
+        message: "Healthy"
+    });
+})
+
+app.get("/publickey", (req, res) => {
+    res.status = 200;
+    res.send({
+        key: publicKey
+    })
+})
 
 app.listen(process.env.PORT || 8081);
